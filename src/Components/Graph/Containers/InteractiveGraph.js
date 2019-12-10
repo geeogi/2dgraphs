ae709dc80070b7272a02ba41694a7485a58c175b
George Bennett <george6bennett@gmail.com>
2019-12-10 20:23:57 +0000

    Remove mobile interactivity

--- src/Components/Graph/Containers/InteractiveGraph.js
+++ src/Components/Graph/Containers/InteractiveGraph.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 const InteractiveGraphBase = styled.div``;
@@ -10,72 +10,33 @@ export const InteractiveGraph = props => {
   const container = useRef();
   const { children } = props;
 
-  useEffect(() => {
-    const handleMouseMove = e => {
-      const rect = e.target.getBoundingClientRect();
-      const x = e.clientX - rect.left; // x position within the element.
-      const y = e.clientY - rect.top; // x position within the element.
-      setActiveX(x);
-      setActiveY(y);
-    };
+  const handleMouseMove = e => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left; // x position within the element.
+    const y = e.clientY - rect.top; // x position within the element.
+    setActiveX(x);
+    setActiveY(y);
+  };
 
-    const handleMouseLeave = () => {
-      setActiveX();
-      setActiveY();
-    };
+  const handleMouseLeave = () => {
+    setActiveX();
+    setActiveY();
+  };
 
-    const handleMouseDown = () => {
-      setIsClicked(true);
-      document.addEventListener("mouseup", () => {
-        setIsClicked(false);
-      });
-    };
-
-    const handleTouchMove = e => {
-      const rect = e.target.getBoundingClientRect();
-      const x = e.clientX - rect.left; // x position within the element.
-      const y = e.clientY - rect.top; // x position within the element.
-      setActiveX(x);
-      setActiveY(y);
-    };
-
-    const handleTouchEnd = () => {
-      setActiveX();
-      setActiveY();
-    };
-
-    const handleTouchStart = () => {
-      setIsClicked(true);
-      document.addEventListener("touchend", () => {
-        setIsClicked(false);
-      });
-    };
-
-    // Attach event listeners to the knob element in active mode (i.e. not "passive")
-    if (container.current) {
-      container.current.addEventListener("touchstart", handleTouchStart, {
-        passive: false
-      });
-      container.current.addEventListener("touchmove", handleTouchMove, {
-        passive: false
-      });
-      container.current.addEventListener("touchend", handleTouchEnd, {
-        passive: false
-      });
-      container.current.addEventListener("mousedown", handleMouseDown, {
-        passive: false
-      });
-      container.current.addEventListener("mousemove", handleMouseMove, {
-        passive: false
-      });
-      container.current.addEventListener("mouseleave", handleMouseLeave, {
-        passive: false
-      });
-    }
-  });
+  const handleMouseDown = () => {
+    setIsClicked(true);
+    document.addEventListener("mouseup", () => {
+      setIsClicked(false);
+    });
+  };
 
   return (
-    <InteractiveGraphBase ref={container}>
+    <InteractiveGraphBase
+      ref={container}
+      onMouseMove={handleMouseMove}
+      onMouseDown={handleMouseDown}
+      onMouseLeave={handleMouseLeave}
+    >
       {children({ activeX, activeY, isClicked })}
     </InteractiveGraphBase>
   );
