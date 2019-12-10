a631dcd3c019d2f0e93ffc99888a363d8642c7ae
George Bennett <george6bennett@gmail.com>
2019-12-10 20:44:37 +0000

    Refine graph margins

--- src/Components/H.js
+++ src/Components/H.js
@@ -4,14 +4,17 @@ import { CONTRAST_COLOR } from "../Data/colors";
 export const H4 = styled.h4`
   color: ${CONTRAST_COLOR};
   padding: 0 8px;
+  margin: 8px 0;
 `;
 
 export const H3 = styled.h3`
   color: ${CONTRAST_COLOR};
   padding: 0 8px;
+  margin: 8px 0;
 `;
 
 export const H1 = styled.h1`
   color: ${CONTRAST_COLOR};
   padding: 0 8px;
+  margin: 16px 0;
 `;
