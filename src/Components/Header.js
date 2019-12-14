3aec7293280bf05da5c3b546b9eeba95da6e61d5
George Bennett <george6bennett@gmail.com>
2019-12-14 11:59:21 +0000

    Restyle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
-  margin: 32px;
+  margin-left: 32px;
+  margin-right: 32px;
+  margin-bottom: 32px;
+  margin-top: 16px;
   @media (max-width: 600px) {
     margin: 32px 0;
   }
