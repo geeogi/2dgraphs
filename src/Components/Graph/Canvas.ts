3310b890175758dd85408dc01792e04e7249bf4e
George Bennett <george6bennett@gmail.com>
2020-01-12 09:45:32 +0000

    reduce graph height on mobile

--- src/Components/Graph/Canvas.ts
+++ src/Components/Graph/Canvas.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MOBILE_SCREEN_WIDTH } from "../../Config/constants";
 
 export const Canvas = styled.canvas`
   user-select: none;
@@ -6,4 +7,7 @@ export const Canvas = styled.canvas`
   display: block;
   width: 100%;
   height: 400px;
+  @media (max-width: ${MOBILE_SCREEN_WIDTH}px) {
+    height: 300px;
+  }
 `;
