e2f897de85cc4c03923ccda754d1d860ca2ea185
George Bennett <george6bennett@gmail.com>
2020-01-12 17:33:41 +0000

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import PRICE_DATA from "./allTime.json";
 import { Button } from "./Components/Base/Button";
 import { Div } from "./Components/Base/Div";
 import { GraphCard } from "./Components/Base/GraphCard";
@@ -7,97 +6,13 @@ import { H1, H3 } from "./Components/Base/H";
 import { Header } from "./Components/Base/Header";
 import { P } from "./Components/Base/P";
 import { Row } from "./Components/Base/Row";
-import { drawGraph2DCanvas } from "./Components/Graph/2DCanvas";
 import { ActiveLegend } from "./Components/Graph/AxisLegend";
 import { Canvas } from "./Components/Graph/Canvas";
 import { ACTIVE_LEGEND } from "./Components/Graph/constants";
-import { positionActiveLegend } from "./Components/Graph/Universal/positionActiveLegend";
-import { positionLabels } from "./Components/Graph/Universal/positionLabels";
-import { calculateGraphValues } from "./Components/Graph/Universal/setup";
-import { drawGraphWebGL } from "./Components/Graph/WebGL";
+import { triggerDraw } from "./draw";
 
 const ACTIVE_LEGEND_WIDTH = ACTIVE_LEGEND.WIDTH;
 const ACTIVE_LEGEND_ID = "active-legend";
-
-// Parse JSON values
-const values: { dateTime: string; price: number }[] = PRICE_DATA.map(value => ({
-  dateTime: value.date,
-  price: value["price(USD)"]
-}));
-
-// Declare render variables
-let noOfDataPoints = 400;
-let currentGraphType: string = "2dcanvas";
-
-// Cache cleanup function to be called if graph is re-rendered
-let cleanup: () => void;
-
-// Declare render method
-const triggerDraw = (
-  newNoOfDataPoints: number = noOfDataPoints,
-  graphType: string = currentGraphType
-) => {
-  // Fetch canvas element
-  let canvasElement: HTMLCanvasElement = document.getElementsByTagName(
-    "canvas"
-  )[0];
-
-  // Replace canvas element if graph type is changed
-  if (graphType !== currentGraphType) {
-    const newCanvasElement = document.createElement("canvas");
-    newCanvasElement.setAttribute(
-      "style",
-      "user-select: none; touch-action: none; display: block; width: 100%; height: 400px;"
-    );
-    canvasElement.insertAdjacentElement("afterend", newCanvasElement);
-    canvasElement.remove();
-    canvasElement = newCanvasElement;
-  }
-
-  // Update render variables cache
-  currentGraphType = graphType;
-  noOfDataPoints = newNoOfDataPoints;
-
-  // Calculate graph coordinates, grid lines  and label values
-  const {
-    priceLabels,
-    dateLabels,
-    xGridLines,
-    yGridLines,
-    points,
-    margin
-  } = calculateGraphValues([...values], newNoOfDataPoints);
-
-  // Call clean up function if applicable
-  if (cleanup) {
-    cleanup();
-  }
-
-  const draw = graphType === "webgl" ? drawGraphWebGL : drawGraph2DCanvas;
-
-  cleanup = draw({
-    canvasElement,
-    points,
-    xGridLines,
-    yGridLines,
-    positionLabels: (canvasElement: HTMLCanvasElement) => {
-      positionLabels(
-        canvasElement,
-        dateLabels,
-        priceLabels,
-        xGridLines,
-        yGridLines,
-        margin
-      );
-    },
-    positionActiveLegend: (
-      canvasElement: HTMLCanvasElement,
-      activeX: number | undefined
-    ) => {
-      positionActiveLegend(canvasElement, activeX, margin, points);
-    }
-  });
-};
 
 function App() {
   // Render on first load
@@ -114,24 +29,21 @@ function App() {
       <GraphCard>
         <H3>Render method:</H3>
         <Row>
-          <Button onClick={() => triggerDraw(noOfDataPoints, "2dcanvas")}>
+          <Button onClick={() => triggerDraw(undefined, "2dcanvas")}>
             2D Canvas
           </Button>
-          <Button onClick={() => triggerDraw(noOfDataPoints, "webgl")}>
-            WebGL
-          </Button>
+          <Button onClick={() => triggerDraw(undefined, "webgl")}>WebGL</Button>
         </Row>
         <H3>Data points:</H3>
         <Row>
           <input
             type="range"
             min={5}
-            max={values.length}
-            defaultValue={noOfDataPoints}
+            max={3000}
+            defaultValue={400}
             step={1}
             onChange={e => triggerDraw(e.target.value as any)}
           ></input>
-          <P>{noOfDataPoints}</P>
         </Row>
         <div>
           <Div position="relative">
---
+++ src/draw.ts
@@ -0,0 +1,88 @@
+import PRICE_DATA from "./allTime.json";
+import { drawGraph2DCanvas } from "./Components/Graph/2DCanvas";
+import { positionActiveLegend } from "./Components/Graph/Universal/positionActiveLegend";
+import { positionLabels } from "./Components/Graph/Universal/positionLabels";
+import { calculateGraphValues } from "./Components/Graph/Universal/setup";
+import { drawGraphWebGL } from "./Components/Graph/WebGL";
+
+// Parse JSON values
+const values: { dateTime: string; price: number }[] = PRICE_DATA.map(value => ({
+  dateTime: value.date,
+  price: value["price(USD)"]
+}));
+
+// Declare render variables
+let noOfDataPoints = 400;
+let currentGraphType: string = "2dcanvas";
+
+// Cache cleanup function to be called if graph is re-rendered
+let cleanup: () => void;
+
+/* MAIN DRAWING METHOD */
+export const triggerDraw = (
+  newNoOfDataPoints: number = noOfDataPoints,
+  graphType: string = currentGraphType
+) => {
+  // Fetch canvas element
+  let canvasElement: HTMLCanvasElement = document.getElementsByTagName(
+    "canvas"
+  )[0];
+
+  // Replace canvas element if graph type has changed
+  if (graphType !== currentGraphType) {
+    const newCanvasElement = document.createElement("canvas");
+    newCanvasElement.setAttribute(
+      "style",
+      "user-select: none; touch-action: none; display: block; width: 100%; height: 400px;"
+    );
+    canvasElement.insertAdjacentElement("afterend", newCanvasElement);
+    canvasElement.remove();
+    canvasElement = newCanvasElement;
+  }
+
+  // Update render variables cache
+  currentGraphType = graphType;
+  noOfDataPoints = newNoOfDataPoints;
+
+  // Calculate graph coordinates, grid lines and label values
+  const {
+    priceLabels,
+    dateLabels,
+    xGridLines,
+    yGridLines,
+    points,
+    margin
+  } = calculateGraphValues([...values], newNoOfDataPoints);
+
+  // Call clean up function if applicable
+  if (cleanup) {
+    cleanup();
+  }
+
+  // Decide which drawing method to use
+  const draw = graphType === "webgl" ? drawGraphWebGL : drawGraph2DCanvas;
+
+  // Draw the canvas
+  cleanup = draw({
+    canvasElement,
+    points,
+    xGridLines,
+    yGridLines,
+    positionLabels: (canvasElement: HTMLCanvasElement) => {
+      positionLabels(
+        canvasElement,
+        dateLabels,
+        priceLabels,
+        xGridLines,
+        yGridLines,
+        margin
+      );
+    },
+    positionActiveLegend: (
+      canvasElement: HTMLCanvasElement,
+      activeX: number | undefined
+    ) => {
+      positionActiveLegend(canvasElement, activeX, margin, points);
+    }
+  });
+};
