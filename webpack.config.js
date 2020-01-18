4f5d53a129aa62bbcd28a89aa68decfb180261a2
George Bennett <george6bennett@gmail.com>
2020-01-18 12:22:26 +0000

    extract crticial CSS during webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
 const path = require("path");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
   mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    }),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, "dist"),
+      src: "index.html",
+      dest: "index.html",
+      css: path.resolve(__dirname, "./src/index.css"),
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 1300,
+      height: 900
+    })
+  ],
   module: {
     rules: [
       {
