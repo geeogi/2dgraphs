fa775e70a21bd21d1f8bc187b403270b7b8e5c0f
George Bennett <george6bennett@gmail.com>
2020-01-25 18:26:49 +0000

    use webgl line shader from D3FC

--- src/Graph/WebGL/WebGLUtils/drawUtils/drawPath.ts
+++ src/Graph/WebGL/WebGLUtils/drawUtils/drawPath.ts
@@ -11,36 +11,48 @@ export const getDrawPathMethod = (
   pathPoints: { x: number; y: number; z?: number }[],
   color: number[]
 ) => {
-  const lineVShader =
-    "uniform vec2 uResolution;" +
-    "uniform vec2 uScale;" +
-    "uniform vec2 uTranslation;" +
-    "attribute vec3 aVertex;" +
-    "attribute vec3 aPrev;" +
-    "attribute vec3 aNext;" +
-    "attribute vec2 aDirection;" +
-    "void main(void) {" +
-    // Declare variables
-    " gl_Position = vec4(aVertex, 1.0);" +
-    " float lineWidth = 1.0;" +
-    // Determine whether this vertex is at the edge of the screen
-    " bool isEdge = aVertex.x == -1.0 || aVertex.x == 1.0;" +
-    // Handle resolution and scale
-    " vec2 uScreen = uResolution * uScale;" +
-    // Calculate miter direction
-    " vec2 AB = normalize(normalize(gl_Position.xy - aPrev.xy) * uScreen);" +
-    " vec2 BC = normalize(normalize(aNext.xy - gl_Position.xy) * uScreen);" +
-    " vec2 tangent = normalize(AB + BC);" +
-    " vec2 miter = isEdge ? vec2(0.0, 1.0) : vec2(-tangent.y, tangent.x);" +
-    // Calculate miter length (max = 5)
-    " vec2 normalA = vec2(-AB.y, AB.x);" +
-    " float miterLength = min(1.0 / dot(miter, normalA), 100.0);" +
-    // Apply miter
-    " gl_Position.xy = gl_Position.xy + (aDirection.x * miter * miterLength * lineWidth) / uScreen.xy;" +
-    // Scale and translate
-    " gl_Position.xy = gl_Position.xy * uScale;" +
-    " gl_Position.xy = gl_Position.xy + uTranslation;" +
-    "}";
+  // https://github.com/d3fc/d3fc/blob/9decc02b958db6ec772073493da7bbbbd94407a7/packages/d3fc-webgl/src/shaders/vertexShaderSnippets.js#L148
+  const lineVShader = `uniform vec2 uResolution; 
+      uniform vec2 uScale; 
+      uniform vec2 uTranslation; 
+      attribute vec2 aCorner; // defines which vertex in the line join we are considering
+      attribute vec2 aCurrent; // Current vertex
+      attribute vec2 aNext; // Next vertex
+      attribute vec2 aPrev; // Previous vertex
+      
+    void main(void) {
+      vec2 uScreen = uResolution * uScale; // Used to normalize vectors
+      gl_Position = vec4(aCurrent.x, aCurrent.y, 0, 1);
+      float uLineWidth = 1.0;
+      vec4 next = vec4(aNext.x, aNext.y, 0, 0);
+      vec4 prev = vec4(aPrev.x, aPrev.y, 0, 0);
+
+      if (all(equal(gl_Position.xy, prev.xy))) {
+          prev.xy = gl_Position.xy + normalize(gl_Position.xy - next.xy);
+      }
+      if (all(equal(gl_Position.xy, next.xy))) {
+          next.xy = gl_Position.xy + normalize(gl_Position.xy - prev.xy);
+      }
+
+      vec2 A = normalize(normalize(gl_Position.xy - prev.xy) * uScreen);
+      vec2 B = normalize(normalize(next.xy - gl_Position.xy) * uScreen);
+      vec2 tangent = normalize(A + B);
+      vec2 miter = vec2(-tangent.y, tangent.x);
+      vec2 normalA = vec2(-A.y, A.x);
+      float miterLength = 1.0 / dot(miter, normalA);
+      vec2 point = normalize(A - B);
+
+      if (miterLength > 10.0 && sign(aCorner.x * dot(miter, point)) > 0.0) {
+          gl_Position.xy = gl_Position.xy - (aCorner.x * aCorner.y * uLineWidth * normalA) / uScreen.xy;
+      } else {
+          gl_Position.xy = gl_Position.xy + (aCorner.x * miter * uLineWidth * miterLength) / uScreen.xy;
+      }
+
+      gl_Position.xy = gl_Position.xy * uScale;
+      gl_Position.xy = gl_Position.xy + uTranslation;
+
+    }
+      `;
 
   const lineFShader = `
     precision mediump float;
@@ -62,26 +74,36 @@ export const getDrawPathMethod = (
   const lineVertices: number[] = [];
   const prevVertices: number[] = [0, 0, 0, 0, 0, 0];
   const nextVertices: number[] = [];
-  const direction: number[] = [];
+  const corner: number[] = [];
 
   pathPoints.forEach((point, index) => {
     const { x, y, z = 0 } = point;
     // Vertex
     lineVertices.push(x, y, z);
     lineVertices.push(x, y, z);
+    lineVertices.push(x, y, z);
+    lineVertices.push(x, y, z);
     // Prev
+    prevVertices.push(x, y, z);
+    prevVertices.push(x, y, z);
     prevVertices.push(x, y, z);
     prevVertices.push(x, y, z);
     // Next
     if (index > 0) {
       nextVertices.push(x, y, z);
       nextVertices.push(x, y, z);
+      nextVertices.push(x, y, z);
+      nextVertices.push(x, y, z);
     }
-    // Miter direction
-    direction.push(1, -1, 1);
-    direction.push(-1, 1, -1);
+    // Miter corner
+    corner.push(-1, -1, 0);
+    corner.push(1, -1, 0);
+    corner.push(-1, 1, 0);
+    corner.push(1, 1, 0);
   });
 
+  nextVertices.push(0, 0, 0);
+  nextVertices.push(0, 0, 0);
   nextVertices.push(0, 0, 0);
   nextVertices.push(0, 0, 0);
 
@@ -89,7 +111,7 @@ export const getDrawPathMethod = (
   const lineVertices_buffer = initArrayBuffer(gl, lineVertices);
   const prevVertices_buffer = initArrayBuffer(gl, prevVertices);
   const nextVertices_buffer = initArrayBuffer(gl, nextVertices);
-  const direction_buffer = initArrayBuffer(gl, direction);
+  const corner_buffer = initArrayBuffer(gl, corner);
 
   return (
     resolution: [number, number],
@@ -100,10 +122,10 @@ export const getDrawPathMethod = (
     gl.useProgram(lineProgram);
 
     // Enable attributes
-    enableAttribute(gl, lineProgram, lineVertices_buffer, "aVertex");
+    enableAttribute(gl, lineProgram, lineVertices_buffer, "aCurrent");
     enableAttribute(gl, lineProgram, prevVertices_buffer, "aPrev");
     enableAttribute(gl, lineProgram, nextVertices_buffer, "aNext");
-    enableAttribute(gl, lineProgram, direction_buffer, "aDirection");
+    enableAttribute(gl, lineProgram, corner_buffer, "aCorner");
 
     // Enable uniforms
     gl.uniform2fv(resolutionUniform, resolution);
@@ -112,6 +134,6 @@ export const getDrawPathMethod = (
     gl.uniform4fv(colorUniform, color);
 
     // Draw the line
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, pathPoints.length * 2);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, pathPoints.length * 4);
   };
 };
